ae1e4cca17a1f2e5786d58f8b8d01c5cdc15d711
StanislavBatura <81703052+StanislavBatura@users.noreply.github.com>
2021-05-18 17:06:57 +0200

    Add files via upload

--- szkola.py
+++ szkola.py
@@ -1,59 +1,110 @@
-COMMANDS = ("wychowawca", "nauczyciel", "uczen", "end", "a")
+lista_nauczycieli = {}
+lista_wychowawcy = {}
+lista_uczni = {}
+lista_klas = []
+lista_klasy_wychowawcy = {}
 
-clas_w = []
-list_wychowawca = {}
-list_nauczyciel = {}
-list_uczen = {}
-list_clas = []
+class Nauczyciel:
+
+    def __init__(self):
+        self.name = ""
+        self.przedmiot = ""
+        self.klasy = []
+
+    def get_data(self):
+        return {"przedmiot": self.przedmiot, "klasy": self.klasy}
+
+    def input_data(self):
+        self.name = input("Podaj Imie i Nazwisko: ")
+        self.przedmiot = input("Podaj przemiot: ")
+
+        while True:
+            klasa = input("Podaj klasę: ")
+            if not klasa:
+                break
+            self.klasy.append(klasa)
+            lista_klas.append(klasa)
+
+class Uczen:
+
+    def __init__(self):
+        self.name = ""
+        self.klasy = ""
+
+    def get_data(self):
+        return
+
+    def input_data(self):
+        self.uczen = input("Podaj imie: ")
+        self.klasy = input("Nazwa klasy: ")
+        if self.klasy not in lista_uczni:
+            lista_uczni[self.klasy] = []
+        lista_uczni[self.klasy].append(self.uczen)
+
+class Wychowawca:
+
+    def __init__(self):
+        self.name = ""
+        self.klasy = ""
+
+    def get_data(self):
+        return
+
+    def input_data(self):
+        self.wychowawca = input("Podaj Imie i Nazwisko: ")
+        while True:
+            self.klasy = input("Podaj klasę: ")
+            if not self.klasy:
+                break
+            if self.klasy not in lista_wychowawcy:
+                lista_wychowawcy[self.klasy] = []
+            lista_wychowawcy[self.klasy].append(self.wychowawca)
+
+
+COMMANDS = ("wychowawca", "nauczyciel", "uczen", "end", "nazwa klasy", "name uczen", "nazwa wychowawcy")
 
 while True:
-    print("1) wychowawca\n2) nauczyciel\n3) uczen\n4) end")
-    commands = input()
-    if commands in COMMANDS:
-        while True:
-            if commands == "uczen":
-                uczen = input("Podaj imie: ")
-                clasa = input("Nazwa klasy: ")
+    command = input("Podaj komendę: ")
+    if not command:
+        break
+    if command not in COMMANDS:
+        continue
+    if command == "end":
+        break
 
-                if uczen in list_uczen:
-                    list_uczen[uczen] += clasa
-                else:
-                    list_uczen[uczen] = clasa
-                print(list_uczen)
+    if command == "nauczyciel":
 
-                if not uczen:
-                    break
+        n = Nauczyciel()
+        n.input_data()
+        lista_nauczycieli[n.name] = n.get_data()
+        print(lista_nauczycieli)
 
-            if commands == "nauczyciel":
-                nauczyciel = input("Podaj imie: ")
-                przedmiot = input("Nazwa przedmiotu: ")
-                if nauczyciel in list_nauczyciel:
-                    list_nauczyciel[nauczyciel] += przedmiot
-                else:
-                    list_nauczyciel[nauczyciel] = przedmiot
-                print(list_nauczyciel)
-                while True:
-                    clasa_n = input("Nazwa klasy: ")
-                    if not clasa_n:
-                        break
+    if command == "uczen":
 
-                    if clasa_n in list_clas:
-                        list_clas += clasa_n
-                    else:
-                        list_clas = clasa_n
+        u = Uczen()
+        u.input_data()
+        lista_uczni[u.name] = u.get_data()
+        print(lista_uczni)
 
-                    print(list_nauczyciel)
-                    print(list_clas)
+    if command == "wychowawca":
 
-            if commands == "wychowawca":
-                wychowawca = input("Podaj imie: ")
-                clasa_w = (input("Nazwa klasy: "))
-                clas_w.append((clasa_w))
-                if wychowawca in list_wychowawca:
-                    list_wychowawca[wychowawca] += clas_w
-                else:
-                    list_wychowawca[wychowawca] = clas_w
-            print(list_wychowawca)
+        w = Wychowawca()
+        w.input_data()
+        lista_wychowawcy[w.name] = w.get_data()
+        print(lista_wychowawcy)
 
-        if commands == "end":
-            break
+    if command == "nazwa klasy":
+        klasa = input("Podaj nazwe klasy: ")
+        for nazwa_klasy, wychowawcy in lista_wychowawcy.items():
+            if klasa == nazwa_klasy:
+                print(wychowawcy)
+
+    if command == "nazwa wychowawcy":
+        wych = input("Podaj imie wychowawcy: ")
+        for nazwa_klasy, wychowawcy in lista_wychowawcy.items():
+            if wychowawcy:
+                if wych in wychowawcy:
+                    print(nazwa_klasy)
+                    for k, f in lista_uczni.items():
+                        if k == nazwa_klasy:
+                            print(k)
