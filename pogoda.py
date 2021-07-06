27ba9e12947a368836758878fbf940f7ced0a62a
StanislavBatura <81703052+StanislavBatura@users.noreply.github.com>
2021-07-06 12:30:56 +0200

    Add files via upload

--- pogoda.py
+++ pogoda.py
@@ -1,28 +1,37 @@
 import requests
 import pprint
-from requests.auth import HTTPBasicAuth
-import datetime
+from datetime import datetime
 
 weathers = []
 
 class Weather:
-    def __init__(self, name, temp_c, localtime):
+    def __init__(self, name, temp_c):
     
-        self.name = name
+        self.name =  name
         self.temp_c = temp_c
-        self.localtime = localtime
+        self.localtime = datetime.today().date()
 
-    def __int__(self):
+    def read(self):
+        with open("pogoda.txt", "r") as file:
+            for line in file.readlines():
+                self.name = line.split(',')[0]
+                self.localtime = line.split(',')[1]
+                self.temp_c = line.split(',')[2].split("\n")[0]
+
+    def __str__(self):
         return f"{self.name}, {self.temp_c}, {self.localtime}"
     
     def save(self):
         with open("pogoda.txt", "a") as file:
-            file.write(str())
+            file.write(str(self.name) + ',' + str(self.localtime) + ',' + str(self.temp_c) + '\n')
     
-#    def read_saldo(self):
-#        with open("pogoda.txt", "r") as file:
-#            for line in file.readlines():
-
+miasto = input("Miasto: ")
+ 
+date = str(input('date (YYYY, MM, D) :'))
+try:
+    dt_start = datetime.strptime(date, '%Y, %m, %d')
+except ValueError:
+    print ("Incorrect format")
 
 class GetDate:
     
@@ -31,13 +40,15 @@ class GetDate:
         self.data = self.get_data()
 
     def get_data(self):
-        r = requests.get('http://api.weatherapi.com/v1/current.json?key=41c0cc296c424df4a7f182052212406&q=Poland&aqi=no')
+        r = requests.get(self.url)
         return r.json()
 
-data = GetDate("http://api.weatherapi.com/v1/current.json?key=41c0cc296c424df4a7f182052212406&q=Poland&aqi=no")
+data = GetDate(f"http://api.weatherapi.com/v1/current.json?key=41c0cc296c424df4a7f182052212406&q= + {miasto} + '&days=' + {date} +  &aqi=no")
 
-for entry in data.data:
-    weather = Weather(entry["location"]["name"], entry["location"]["localtime"], entry["condition"]["temp_c"])
-    weathers.append(weather)
+print(data.data["current"]["temp_c"])
 
-print(weathers[-1].name)
+weather = Weather(miasto, data.data["current"]["temp_c"])
+weathers.append(weather)
+
+
+weather.save()
